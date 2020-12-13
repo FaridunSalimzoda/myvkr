3f8322de4522524a56a3933d9d7c667d51ce7bd6
faridun <farid-90@bk.ru>
2020-12-13 12:57:58 +0300

    Создал бд(не доконца) не знаю правильно ли

--- app/cours/admin.py
+++ app/cours/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import kursu
-from .models import addtopic
+from .models import kursu, addtopic, AssignedCoursesTable, UserTable
 # Register your models here.
 
 
 admin.site.register(kursu),
-admin.site.register(addtopic)
+admin.site.register(addtopic),
+admin.site.register(AssignedCoursesTable),
+admin.site.register(UserTable)
--- app/cours/models.py
+++ app/cours/models.py
@@ -33,3 +33,56 @@ class addtopic(models.Model):
 
         verbose_name = 'Тему'
         verbose_name_plural = 'Темы'
+
+
+class UserTable(models.Model):
+    Surname = models.CharField('Фамилия', max_length=30)
+    Name = models.CharField('Имя', max_length=30)
+    Patromic = models.CharField('Отчество', max_length=40)
+    email = models.CharField('E-mail', max_length=40)
+    user_status = models.CharField('Статус', max_length=30)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+class AssignedCoursesTable(models.Model):
+    id_course = models.ManyToManyField(kursu, on_delete=models.CASCADE)
+    id_user = models.ManyToManyField(UserTable, on_delete=models.CASCADE)
+
+class TestTable(models.Model):
+    id_topic = models.ForeignKey(addtopic, on_delete=models.CASCADE)
+    test_name = models.CharField('Название теста', max_length=50)
+    time = models.TimeField('Время прохождение')
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+
+class QuestionsTable(models.Model):
+    id_test = models.ForeignKey(TestTable, on_delete=models.CASCADE)
+    text = models.CharField('Текст вопроса', max_length=70)
+    number = models.IntegerField('Порядковый номер вопроса') # тут я совневаюсь
+    ball = models.IntegerField('Балл за ответ')
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+class AnswerTable(models.Model):
+    id_question = models.ForeignKey(QuestionsTable, on_delete=models.CASCADE)
+    text_answer = models.TextField('Ответ на вопрос', max_length=60)
+    try_answer = models.BooleanField("Правильный ответ", max_length=60)
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+class ExamTable(models.Model):
+    id_questions = models.ManyToManyField(QuestionsTable, on_delete=models.CASCADE)
+    id_answer = models.ManyToManyField(AnswerTable, on_delete=models.CASCADE)
+
+class ResultsTable(models.Model):
+    id_test = models.ManyToManyField(TestTable, on_delete=models.CASCADE)
+    id_user = models.ManyToManyField(UserTable, on_delete=models.CASCADE)
+    id_exam = models.ManyToManyField(ExamTable, on_delete=models.CASCADE)
+    estimation = models.IntegerField()
+    timer = models.TimeField()
+
